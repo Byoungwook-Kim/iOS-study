fd9fb14581dfae30a6015373b88018cc3b67b363
Byoungwook-Kim <Crucrat@gmail.com>
2019-11-09 15:49:31 +0900

    TabStudy

---
+++ TabBar/TabBar/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  TabBar
+//
+//  Created by Byoung_wook on 04/11/2019.
+//  Copyright © 2019 Byoung_wook. All rights reserved.
+//
+
+import UIKit
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //1
+        let title = UILabel(frame: CGRect(x: 0, y:100, width: 100, height: 30))
+        //2
+        title.text = "세번째 탭"
+        title.textColor = UIColor.red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        //3
+        title.sizeToFit()
+        //
+        title.center.x = self.view.frame.width / 2
+        //5
+        self.view.addSubview(title)
+        
+        
+    }
+}
+
