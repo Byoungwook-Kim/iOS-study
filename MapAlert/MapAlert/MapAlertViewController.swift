4a79b31f9e9120686a00977461aa95469ce3d64a
Byoungwook-Kim <Crucrat@gmail.com>
2019-11-14 23:25:06 +0900

    ImageAlert

--- MapAlert/MapAlert/MapAlertViewController.swift
+++ MapAlert/MapAlert/MapAlertViewController.swift
@@ -41,5 +41,29 @@ class MapAlertViewController: UIViewController {
         
         self.present(alert, animated: false)
         
+        let imageBtn = UIButton(type: .system)
+           // 이미지 알림창 버튼 속성 설정
+           imageBtn.frame = CGRect(x: 0, y: 200, width: 100, height: 30)
+           imageBtn.center.x = self.view.frame.width / 2
+           imageBtn.setTitle("Image Alert", for: .normal)
+           imageBtn.addTarget(self, action: #selector(imageAlert(_:)), for: .touchUpInside)
+           
+           self.view.addSubview(imageBtn)
+    }
+    @objc func imageAlert(_ sender: Any){
+        // 경고창 객체를 생성하고, OK 버튼을 추가한다.
+           let alert = UIAlertController(title:nil,
+                                         message: "이번 글의 평점은 다음과 같습니다.",
+                                         preferredStyle: .alert)
+           
+           let okAction = UIAlertAction(title: "OK", style: .default)
+           alert.addAction(okAction)
+           
+           // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러를 생성하고, 알림창에 등록한다.
+           let contentVC = ImageViewController()
+           alert.setValue(contentVC, forKeyPath: "contentViewController")
+           
+           self.present(alert, animated: false)
+        
     }
 }
