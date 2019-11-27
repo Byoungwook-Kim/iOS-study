e1aeb99b40011f7925d0e4b0492debc363c35543
Byoungwook-Kim <Crucrat@gmail.com>
2019-11-28 00:00:53 +0900

    SideBarSet

---
+++ SideBar/SideBar/FrontViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FrontViewController.swift
+//  SideBar
+//
+//  Created by Byoung_wook on 27/11/2019.
+//  Copyright © 2019 Byoung_wook. All rights reserved.
+//
+
+import UIKit
+class FrontViewController : UIViewController {
+    
+    @IBOutlet var sideBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SideBar/SideBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideBar
+//
+//  Created by Byoung_wook on 27/11/2019.
+//  Copyright © 2019 Byoung_wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
