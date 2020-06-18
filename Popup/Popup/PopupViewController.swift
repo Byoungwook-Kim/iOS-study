d1d8718c1413e5a8f41f0b74248c5a06b93b8c66
Byoungwook-Kim <Crucrat@gmail.com>
2020-06-18 17:48:38 +0900

    PopUp Study

---
+++ Popup/Popup/PopupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PopupViewController.swift
+//  Popup
+//
+//  Created by Byoung_wook on 2020/06/17.
+//  Copyright © 2020 Byoung_wook. All rights reserved.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Popup/Popup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Popup
+//
+//  Created by Byoung_wook on 2020/06/17.
+//  Copyright © 2020 Byoung_wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
